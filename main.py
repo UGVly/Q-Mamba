4c5d5a8cba25bd03b85c0faf6b91cdbf73a87d39
UGVly <2174273465@qq.com>
2024-08-05 17:49:29 +0800

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+import torch
+
+from options import get_options
+
+def run(cfg):
+    pass
+    
+
+    
+
+if __name__ == '__main__':
+    cfg = get_options()
+    run(cfg)
--- options.py
+++ options.py
@@ -3,11 +3,24 @@ import time
 import argparse
 import torch
 
-def options():
+# return cfg
+def get_options(args=None):
     parser = argparse.ArgumentParser(description = "Start q-mamba learning")
 
     # mode
     parser.add_argument('--mode', type = 'str', default = 'train', help = 'train or test')
-    parser.add_argument('--device', type = 'str', default = 'cuda',help = 'cuda or cpu')
+    parser.add_argument('--device', type = 'str', default = 'cpu', choices = ["cuda", "gpu", "cpu"], help = 'cuda or cpu')
+    parser.add_argument('--seed', type = 'int', default = 0, help = 'random seed')
+    
+    # data
+    
+    
+    
+    opts = parser.parse_args(args)
+    
+    opts.distributed = False
+    
+    return opts
+
 
 
